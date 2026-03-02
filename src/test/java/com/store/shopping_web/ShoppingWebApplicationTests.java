5902c0807bc4ac4bba063b94ef0e3549aaa4c0bb
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2026-03-02 20:35:27 +0700

    first commit

---
+++ src/test/java/com/store/shopping_web/ShoppingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.store.shopping_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
